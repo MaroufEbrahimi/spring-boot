b1dfdf0cf4ba8e8b70e3defccbb3fe087381f3f7
Marouf <maroufibrahimi7264@gmail.com>
2024-01-14 13:26:35 +0430

    input text and display on page

--- src/main/java/com/example/springbootfirstwebapplication/controller/LoginController.java
+++ src/main/java/com/example/springbootfirstwebapplication/controller/LoginController.java
@@ -3,14 +3,21 @@ package com.example.springbootfirstwebapplication.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login")
-    public String loginMessage(ModelMap model) {
-//        model.put("name", name);
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showMessage(ModelMap model) {
         return "login";
+    }
+
+    @RequestMapping(value="/login", method = RequestMethod.POST)
+    public String loginMessage(ModelMap model, @RequestParam String name) {
+        model.put("name", name);
+        return "welcome";
     }
 }
 
