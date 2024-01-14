b721f435d922a3abec87cd0cbf55e89c71679da0
Marouf <maroufibrahimi7264@gmail.com>
2024-01-14 19:29:26 +0430

    added TodoService for manage todos

---
+++ src/main/java/com/example/springbootfirstwebapplication/sercive/TodoService.java
@@ -0,0 +1,45 @@
+package com.example.springbootfirstwebapplication.sercive;
+
+import com.example.springbootfirstwebapplication.model.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<Todo>();
+    private static int todoCount = 3;
+
+    static {
+        todos.add(new Todo(1, "famous", "todo for new spring", new Date(), false));
+        todos.add(new Todo(2, "Marouf", "spring with Marouf", new Date(), false));
+        todos.add(new Todo(3, "ali", "full stack tutorials with ali", new Date(), false));
+    }
+
+    public List<Todo> retrieveTodos(String user) {
+        List<Todo> filteredTodos = new ArrayList<Todo>();
+        for (Todo todo : todos) {
+            if (todo.getUser().equals(user)) {
+                filteredTodos.add(todo);
+            }
+        }
+        return filteredTodos;
+    }
+
+    public void addTodo(String name, String desc, Date targetDate, boolean isDone) {
+        todos.add(new Todo(++todoCount, name, desc, targetDate, isDone));
+    }
+
+    public void deleteTodo(int id) {
+        Iterator<Todo> iterator = todos.iterator();
+        while (iterator.hasNext()) {
+            Todo todo = iterator.next();
+            if (todo.getId() == id) {
+                iterator.remove();
+            }
+        }
+    }
+}
