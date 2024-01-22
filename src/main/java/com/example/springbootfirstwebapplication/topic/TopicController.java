96a390beda33e18d47836b100cbd3434dba329d8
Marouf <maroufibrahimi7264@gmail.com>
2024-01-22 16:26:25 +0430

    creating a business service

--- src/main/java/com/example/springbootfirstwebapplication/topic/TopicController.java
+++ src/main/java/com/example/springbootfirstwebapplication/topic/TopicController.java
@@ -1,20 +1,19 @@
 package com.example.springbootfirstwebapplication.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("Spring", "Spring Frame", "Spring Frame Desc"),
-                new Topic("Java", "Java Tutorials", "Java Tutorials Desc"),
-                new Topic("React", "React Tutorials", "React Tutorials Desc")
-        );
+        return topicService.getAllTopics();
     }
 }
---
+++ src/main/java/com/example/springbootfirstwebapplication/topic/TopicService.java
@@ -0,0 +1,20 @@
+package com.example.springbootfirstwebapplication.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("Spring", "Spring Frame", "Spring Frame Desc"),
+            new Topic("Java", "Java Tutorials", "Java Tutorials Desc"),
+            new Topic("React", "React Tutorials", "React Tutorials Desc")
+    );
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+}
