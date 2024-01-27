5b515b104e392f5aabb92dae482542c8c18e3584
Marouf <maroufibrahimi7264@gmail.com>
2024-01-27 19:39:24 +0430

    create Student on Test class

--- src/main/java/com/example/springbootfirstwebapplication/studentDAL/entities/Student.java
+++ src/main/java/com/example/springbootfirstwebapplication/studentDAL/entities/Student.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "sname")
     private String name;
--- src/test/java/com/example/springbootfirstwebapplication/SpringBootFirstWebApplicationTests.java
+++ src/test/java/com/example/springbootfirstwebapplication/SpringBootFirstWebApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.springbootfirstwebapplication;
 
+import com.example.springbootfirstwebapplication.studentDAL.entities.Student;
+import com.example.springbootfirstwebapplication.studentDAL.repos.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootFirstWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private StudentRepository repo;
+
+    @Test
+    void testCreateStudent() {
+        Student student = new Student();
+        student.setName("Marouf");
+        student.setCourse("Spring with data JPA");
+        student.setFee(50d);
+        repo.save(student);
+    }
 
 }
