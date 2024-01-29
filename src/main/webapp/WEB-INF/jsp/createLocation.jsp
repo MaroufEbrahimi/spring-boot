8d32bf690f266d02b244776328994c1dcdf6f8f7
Marouf <maroufibrahimi7264@gmail.com>
2024-01-29 06:21:01 +0430

    create the controller & context-path of app

---
+++ src/main/webapp/WEB-INF/jsp/createLocation.jsp
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Web Project | Create Location</title>
+</head>
+<body>
+<form action="" method="">
+    ID: <input type="text" name="id"/>
+    Code: <input type="text" name="code"/>
+    Name: <input type="text" name="name"/>
+    Type: Urban <input type="radio" name="type"/>
+    Rural <input type="radio" name="type"/>
+    <input type="submit" value="Save"/>
+</form>
+</body>
+</html>
--- src/test/java/com/example/springbootfirstwebapplication/SpringBootFirstWebApplicationTests.java
+++ src/test/java/com/example/springbootfirstwebapplication/SpringBootFirstWebApplicationTests.java
@@ -1,42 +1,12 @@
 package com.example.springbootfirstwebapplication;
 
-import com.example.springbootfirstwebapplication.studentDAL.entities.Student;
-import com.example.springbootfirstwebapplication.studentDAL.repos.StudentRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootFirstWebApplicationTests {
 
-    @Autowired
-    private StudentRepository repo;
-
     @Test
-    void testCreateStudent() {
-        Student student = new Student();
-        student.setName("Marouf");
-        student.setCourse("Spring with data JPA");
-        student.setFee(50d);
-        repo.save(student);
-    }
-
-    @Test
-    void testFindStudentById() {
-        Student student = repo.findById(1L).get();
-        System.out.println(student);
-    }
-
-    @Test
-    void testUpdateStudent() {
-        Student student = repo.findById(1L).get();
-        student.setName("Ahmad");
-        repo.save(student);
-    }
-
-    @Test
-    void testDeleteStudent() {
-        Student student = repo.findById(1L).get();
-        repo.delete(student);
+    void contextLoads() {
     }
 }
