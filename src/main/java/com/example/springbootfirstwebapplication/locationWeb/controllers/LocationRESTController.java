44a40c14aa1b18680193c908fba38f2cf75159ce
Marouf <maroufibrahimi7264@gmail.com>
2024-02-03 14:03:14 +0430

    add REST for update & delete Location

--- src/main/java/com/example/springbootfirstwebapplication/locationWeb/controllers/LocationRESTController.java
+++ src/main/java/com/example/springbootfirstwebapplication/locationWeb/controllers/LocationRESTController.java
@@ -23,4 +23,33 @@ public class LocationRESTController {
     public Location createLocation(@RequestBody Location location) {
         return locationRepository.save(location);
     }
+
+    @PutMapping
+    public Location updateLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLocation(@PathVariable("id") int id) {
+        locationRepository.deleteById(id);
+    }
+
+    @GetMapping("/{id}")
+    public Location getLocationById(@PathVariable("id") int id) {
+        return locationRepository.findById(id).orElse(null);
+    }
+
+    /*
+     *
+     * Second Method for Get Location By ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Location> getLocationById2(@PathVariable("id") int id) {
+        Optional<Location> locationOptional = locationRepository.findById(id);
+
+        return locationOptional
+                .map(location -> new ResponseEntity<>(location, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+     */
+
 }
